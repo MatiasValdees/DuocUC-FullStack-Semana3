0afc9998c2ac3dd11347d20b3cbb90550b135250
MatiasValdees <matias.valdesr3@gmail.com>
2025-03-29 19:01:25 -0300

    Global advice microservicio2

---
+++ microservicio-viaje-mascota/src/main/java/cl/duoc/week3/config/exceptions/ExceptionResponse.java
@@ -0,0 +1,19 @@
+package cl.duoc.week3.config.exceptions;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ExceptionResponse {
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
+    private LocalDateTime timestamp;
+    private int status;
+    private String error;
+    private String message;
+}
